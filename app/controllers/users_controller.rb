f828ed63fa753dcf3c61e6612cc067abe9c3d528
Uchi8610 <b1900686@planet.kanazawa-it.ac.jp>
2020-06-15 15:14:02 +0900

    complete-UsersContoller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,24 +6,39 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @action = "新規登録"
   end
 
   def show
     @user = User.find(params[:id])
   end
-
+  
   def create
+    user_form("new")
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    @action = "編集"
+  end
+
+  def update
+    user_form("edit")
+  end
+
+  def user_form(foo)
     @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
       flash[:alert] = "エラーが発生しました。#{@user.errors.full_messages}"
-      render 'new'
+      render foo
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
   end
 
   private
